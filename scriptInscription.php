c047e9301a319a651dd4ae29a67050c2cda99d25
Eric Hemmerlin <ehemmerlin@users.noreply.github.com>
2019-10-15 11:51:25 +0200

    feat: add sql scripts and sign up/in

--- scriptInscription.php
+++ scriptInscription.php
@@ -5,12 +5,19 @@ $pseudo = $_POST['pseudo'];
 $motDePasse = $_POST['motDePasse'];
 
 // Requête préparée pour empêcher les injections SQL
-$requete = $bdd->prepare("SELECT pseudo FROM utilisateurs WHERE pseudo=:pseudo AND motDePasse=:motdepasse");
+$requete = $bdd->prepare("SELECT pseudo from utilisateurs WHERE pseudo=:pseudo");
 $requete->bindValue(':pseudo', $pseudo, PDO::PARAM_STR);
-$requete->bindValue(':motdepasse', $motDePasse, PDO::PARAM_STR);
 $requete->execute();
 
-echo $requete->fetch() ? 'Connexion réussie.' : 'Connexion échouée';
-// TODO : si l'utilisateur n'existe pas le créer avec une requête INSERT INTO
-
+if( $requete->fetch() ) {
+    echo 'Pseudo déjà utilisé.';
+} else {
+    echo 'Inscription validée';
+    $request = $bdd->prepare("INSERT INTO utilisateurs (pseudo, motDePasse) VALUES (:pseudo,:motDePasse)");
+    
+    $request->bindValue(':pseudo', $pseudo, PDO::PARAM_STR);
+    $request->bindValue(':motDePasse', $motDePasse, PDO::PARAM_STR);
+    $request->execute();
+    $request->closeCursor();
+}
 $requete->closeCursor();
